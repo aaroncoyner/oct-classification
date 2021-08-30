02593cef46bc195a0c5df5965714c383f0ca32a8
Aaron Coyner <coyner@ohsu.edu>
2021-08-30 15:56:30 -0700

    Add relative file paths

--- 1_partition_data.R
+++ 1_partition_data.R
@@ -1,14 +1,17 @@
 library(groupdata2)
 library(tidyverse)
 library(tools)
+library(utils)
 
 
+relative_path <- file.path('data', 'images')
 
-all_data <- list.files('./data/images') %>%
+all_data <- list.files(relative_path) %>%
     as.data.frame() %>%
     rename(filepath = '.') %>%
     separate(filepath, c('annotation', 'studyinstanceuid', 'image_number', 'ext'), remove = FALSE) %>%
-    mutate(across(c('studyinstanceuid', 'annotation'), as.factor)) %>%
+    mutate(across(c('studyinstanceuid', 'annotation'), as.factor),
+           filepath = file.path(relative_path, filepath)) %>%
     select(filepath, studyinstanceuid, annotation)
 
 
@@ -18,9 +21,14 @@ partitioned_data <- all_data %>%
     partition(p = c(0.7, 0.1),
               id_col = 'studyinstanceuid')
 
-train_data <- partitioned_data[[1]]
-val_data <- partitioned_data[[2]]
-test_data <- partitioned_data[[3]]
+train_data <- partitioned_data[[1]] %>%
+    select(filepath, studyinstanceuid, annotation)
+
+val_data <- partitioned_data[[2]] %>%
+    select(filepath, studyinstanceuid, annotation)
+
+test_data <- partitioned_data[[3]] %>%
+    select(filepath, studyinstanceuid, annotation)
 
 write_csv(train_data, './data/training.csv')
 write_csv(val_data, './data/validation.csv')
